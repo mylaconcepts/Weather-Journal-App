180108a285bca6b3d92ef72d5215705ffdf73f98
Martine Yemma <70291070+mylaconcepts@users.noreply.github.com>
2021-03-03 20:19:08 -0800

    styled and responsive
    Added styling and breakpoint for responsiveness

--- website/app.js
+++ website/app.js
@@ -9,7 +9,7 @@ const apiKey = '&appid=af8187d52ec3361fbb06038a90a2ef8b&units=imperial'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
 /* Function called by event listener */
